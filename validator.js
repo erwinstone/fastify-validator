056522ea0dd2ae1aff4272c79f1bcc7d11b95540
Erwin Stone <erwinstone@pm.me>
2022-08-10 11:53:10 +0700

    Initial commit

---
+++ validator.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const validator = require('simple-body-validator')
+
+function fastifyValidator(fastify, _options, next) {
+	fastify.decorateRequest('validator', { getter: () => validator })
+	next()
+}
+
+module.exports = fp(fastifyValidator, {
+	fastify: '4.x',
+	name: 'fastify-validator',
+})
